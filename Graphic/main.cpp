279efb530143c3c44fc255a275abc3359a068c5f
Ptr <frantic89@o2.pl>
2012-08-13 18:46:04 +0200

    Set up a eclipse project. Hello World

---
+++ Graphic/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 13-08-2012
+ *      Author: Ptr
+ */
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	using namespace std;
+	cout << "HEllo, I'm Graphic" << endl;
+	return 0;
+}
+
+
+
